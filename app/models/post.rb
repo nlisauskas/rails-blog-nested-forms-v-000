1e51ee05a7d72345622c58dc915873cd5c7443a1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-27 17:06:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,8 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags , reject_if: proc { |attributes| attributes['name'].blank?}
 
   validates_presence_of :name, :content
 
-  def tags_attributes(tag_hash)
-    if tag_hash[:name].present?
-      Tag.find_or_create_by(tag_hash[:name])
-    end
-  end
 end
